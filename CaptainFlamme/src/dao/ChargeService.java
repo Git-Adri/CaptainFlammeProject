aa8993e295512d0eda5e70fed63aaa36bef83248
CHENTing <krystal_0902@icloud.com>
2017-12-11 14:41:22 +0100

    add dao and servlet

---
+++ CaptainFlamme/src/dao/ChargeService.java
@@ -0,0 +1,14 @@
+package dao;
+
+public class ChargeService {
+	
+	public static DBmanager db = DBmanager.createInstance();
+	
+	public int closeCharge(String id){
+		String sql = "select ";
+		
+		return 0;
+	}
+	
+
+}
